0d1d970bb2bc700b646c190540f1eb9c1ba9d456
Bruno Ortiz <brunoort@gmail.com>
2019-07-03 10:08:24 -0300

    last

--- src/main/java/br/com/institutointegra/sample/resource/PersonResource.java
+++ src/main/java/br/com/institutointegra/sample/resource/PersonResource.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +25,18 @@ public class PersonResource {
 	public List<Person> getAll(){
 		return this.personService.getAll();
 	}
+	
+	@PostMapping("/")
+	public Person save(@RequestBody Person person) {
+		return this.personService.save(person);
+	}
+	
+	@PutMapping("/{id}")
+	public Person update(
+			@PathVariable(name = "id", required = false) Long idPerson, 
+			@RequestBody Person person
+			) {
+		return this.personService.save(person);
+	}
+	
 }
